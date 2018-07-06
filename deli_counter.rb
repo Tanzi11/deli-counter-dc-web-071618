ffada7e0ed86c20c03f0765d4f7dbf7fbbdca918
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-07-06 22:34:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,14 @@
 katz_deli = []
+def line(katz_deli)
+puts "The line is currently empty." if  katz_deli.count == 0
+x=0
+counter ="The line is currently:" 
+while x < katz_deli.length
+ counter+=" #{x+1}.#{katz_deli[x]}"
+ x+=1
+end
+puts counter 
+end
 def take_a_number(katz_deli, name)
 if  katz_deli.count == 0
   puts "The line is currently empty." 
