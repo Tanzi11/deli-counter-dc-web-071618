3121171c8e2358a5df95fa0be6b815589d0d0d6c
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-07-06 22:32:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,6 @@
 
 def take_a_number(katz_deli, name)
-puts "The line is currently empty." 
-if  katz_deli.count == 0
+puts "The line is currently empty." if  katz_deli.count == 0
   katz_deli << name
   puts "Welcome,#{name}. You are number #{katz_deli.count} in line."
 end
